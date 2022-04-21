3829967cfcfac28e1ccc87285207050585298bce
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-04-21 12:15:08 +0500

    Add project files.

---
+++ LeetcodeSolutions/GroupAnagrams.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolutions
+{
+    internal class GroupAnagrams
+    {
+        public IList<IList<string>> GroupAnagram(string[] words)
+        {
+            Dictionary<string, IList<string>> anagramPairs = new
+                Dictionary<string, IList<string>>();
+
+            for (int index = 0; index < words.Length; index++)
+            {
+                var wordCharArray = words[index].ToArray();
+
+                Array.Sort(wordCharArray);
+
+                string sortedWord = wordCharArray.ToString();
+                
+                if (anagramPairs.ContainsKey(sortedWord))
+                    anagramPairs[sortedWord].Add(words[index]);
+                
+                else
+                    anagramPairs.Add(
+                        key: sortedWord, 
+                        value: new List<string> { words[index] });
+            }
+
+            return anagramPairs.Values.ToList();
+        }
+    }
+}
---
+++ LeetcodeSolutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetcodeSolutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
