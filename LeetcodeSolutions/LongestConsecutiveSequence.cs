fd7f53d0049e28dc224fb5d2ef8091171e19eb4f
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-04-21 21:43:48 +0500

    Longest Consecutive Sequence

---
+++ LeetcodeSolutions/LongestConsecutiveSequence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolutions
+{
+    internal class LongestConsecutiveSequence
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            var hashSet = new HashSet<int>(nums);
+
+            int maxLength = 0;
+
+            foreach(var num in nums)
+            {
+                if (hashSet.Contains(num - 1))
+                    continue;
+                else
+                {
+                    int start = num;
+                    int length = 0;
+
+                    while(hashSet.Contains(start))
+                    {
+                        start++;
+                        length++;
+                    }
+                    maxLength = Math.Max(maxLength, length);
+                }
+            }
+            
+            return maxLength;
+        }
+    }
+}
--- LeetcodeSolutions/Program.cs
+++ LeetcodeSolutions/Program.cs
@@ -6,7 +6,12 @@ namespace LeetcodeSolutions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
+
+            LongestConsecutiveSequence longestConsecutiveSequence =
+                new LongestConsecutiveSequence();
+
+            int result = longestConsecutiveSequence.LongestConsecutive(nums);
         }
     }
 }
